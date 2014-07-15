440aa02dcd6337f3a6d9e486476ce4f3244bee36
新之助不裸不好看 <173983476@qq.com>
2014-07-15 10:13:17 +0800

    .

--- Sites/js/taphold.js
+++ Sites/js/taphold.js
@@ -9,9 +9,22 @@ $(document).on("pageinit","#homePage",function(){
 	$("#mainContent ul li a").on("tap",function(){
 		count++; //实现再次tap即消去#function的效果,count==2,再次点击时count==3
 		if(count>2){
-			$('#footer').show();
-			$("#function").slideUp();
+			$('#footer').slideDown(1000);
+			$("#function").hide();
 			count=0;
 		};
 	});
+	//以上代码实现点击时的#function的弹框效果
+
+
+function showFileName()
+      {
+        var file = document.getElementById("upload");
+        for(var i = 0, j = file.files.length; i < j; i++)
+            {
+               alert(file.files[i].name);
+                            };
+            };
+	$("#header form input#upload").hide();
+	$("#header input.tap").on("tap",showFileName());
 });
